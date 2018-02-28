05c6000f075c3776cebe590661f44126fd958ca5
topilski <atopilski@gmail.com>
2018-02-28 21:17:23 +0300

    1.15.4

--- config/public_settings.js
+++ config/public_settings.js
@@ -3,7 +3,7 @@ module.exports = {
     project: {
         name: 'FastoRedis',
         name_lowercase: 'fastoredis',
-        version: '1.15.3',
+        version: '1.15.4',
         domain: 'fastoredis.com'
     },
 
