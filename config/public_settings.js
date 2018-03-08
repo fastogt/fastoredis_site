3eff828fddb91ee5ca56872de5a9a63826f4ce29
topilski <atopilski@gmail.com>
2018-03-08 09:46:59 +0300

    Open trials

--- config/public_settings.js
+++ config/public_settings.js
@@ -3,7 +3,7 @@ module.exports = {
     project: {
         name: 'FastoRedis',
         name_lowercase: 'fastoredis',
-        version: '1.15.4',
+        version: '1.15.5',
         domain: 'fastoredis.com'
     },
 
