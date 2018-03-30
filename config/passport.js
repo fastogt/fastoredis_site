5297a17d87a556875be02c2c8755f361f601c770
topilski <atopilski@gmail.com>
2018-03-30 04:59:29 +0300

    Delete account

--- config/passport.js
+++ config/passport.js
@@ -11,6 +11,7 @@ var configAuth = require('./auth'); // use this one for testing
 function validateEmail(email, done) {
     var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     var is_valid = re.test(email);
+
     if (!is_valid) {
         done('Invalid email input.');
         return;
