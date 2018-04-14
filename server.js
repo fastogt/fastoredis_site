cc49ab20286bd8d76aae3e80ad8a373a79b10287
topilski <atopilski@gmail.com>
2018-04-14 18:29:10 +0300

    AWS image

--- server.js
+++ server.js
@@ -96,13 +96,13 @@ app.locals.back_end = {
     socketio_port: settings_config.socketio_port
 };
 
-// fast spring
+// fastspring
 app.locals.fastspring_config = {
     login: settings_config.fastspring_login,
     password: settings_config.fastspring_password
 };
 
-// mailer lite
+// mailerlite
 app.locals.mailer_lite_config = {
     group: settings_config.mailer_lite_group
 };
@@ -476,6 +476,7 @@ function ban_user(args, opt, callback) {
 }
 
 
+// handlers
 json_rpc2_server.expose('version', version);
 json_rpc2_server.expose('statistic', statistic);
 json_rpc2_server.expose('is_subscribed', is_subscribed);
