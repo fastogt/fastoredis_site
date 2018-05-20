c860a9b833e77214877ba1c09231b0b5d5fa393d
topilski <atopilski@gmail.com>
2018-05-20 16:48:40 +0300

    Hot fix

--- app/models/user_backups.js
+++ app/models/user_backups.js
@@ -1,4 +1,5 @@
 var userSchema = require('./user');
+var mongoose = require('mongoose');
 
 // create the model for users and expose it to our app
 module.exports = mongoose.model('UserBackups', userSchema);
