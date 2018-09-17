b8e2633a6e27f63865d928a508e37dfb103de50f
topilski <atopilski@gmail.com>
2018-09-17 09:32:11 +0300

    Release

--- config/public_settings.js
+++ config/public_settings.js
@@ -3,7 +3,7 @@ module.exports = {
     project: {
         name: 'FastoRedis',
         name_lowercase: 'fastoredis',
-        version: '1.17.1',
+        version: '1.18.0',
         domain: 'fastoredis.com',
         trial_days: 30,
         price_per_month: 3.99,
