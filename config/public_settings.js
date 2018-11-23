ad6a4c5a1325b593a66dbbd02da154de48e10a5a
topilski <atopilski@gmail.com>
2018-11-23 08:25:45 +0300

    Pro version

--- config/public_settings.js
+++ config/public_settings.js
@@ -7,6 +7,7 @@ module.exports = {
         domain: 'fastoredis.com',
         trial_days: 30,
         price_per_month: 4.99,
+        price_per_6_month: 26.99,
         price_per_year: 49.99
     },
 
