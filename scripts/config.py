d6bd5b711298353c055dd65d9d73838bec96046f
topilski <atopilski@gmail.com>
2019-04-28 17:49:34 +0300

    New release

--- scripts/config.py
+++ scripts/config.py
@@ -18,7 +18,8 @@ versions = ['1.22.3.0',
             '2.1.3.0',
             '2.1.4.0',
             '2.1.5.0',
-            '2.2.0.0']
+            '2.2.0.0',
+            '2.2.1.0']
 
 welcome_file_name = 'base'
 welcome_file_pro_name = 'base_pro'
